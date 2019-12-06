d344dd3c32785c71a233ccb55c604489eabfaf8c
alexpeachey <alex.peachey@gmail.com>
2019-12-05 19:04:42 -0800

    Small refactor

--- lib/advent/day5b.ex
+++ lib/advent/day5b.ex
@@ -13,7 +13,7 @@ defmodule Advent.Day5b do
   def run(%{mem: mem, ptr: ptr} = state) do
     [opcode | params] = Enum.slice(mem, ptr..-1)
 
-    [pad(opcode) | params]
+    [parse(pad(opcode)) | params]
     |> process(state)
     |> case do
       {:ok, state} -> run(state)
@@ -26,98 +26,46 @@ defmodule Advent.Day5b do
     IO.inspect(reason)
   end
 
-  def process(["00099" | _], state) do
+  def process([{_a, _b, _c, "99"} | _], state) do
     {:stop, :halt, state}
   end
 
-  def process(
-        [
-          <<_a::bytes-size(1)>> <> <<b::bytes-size(1)>> <> <<c::bytes-size(1)>> <> "01",
-          p1,
-          p2,
-          dest | _
-        ],
-        %{mem: mem, ptr: ptr}
-      ) do
+  def process([{_a, b, c, "01"}, p1, p2, dest | _], %{mem: mem, ptr: ptr}) do
     mem = compute(mem, {c, p1}, {b, p2}, dest, &Kernel.+/2)
     {:ok, %{mem: mem, ptr: ptr + 4}}
   end
 
-  def process(
-        [
-          <<_a::bytes-size(1)>> <> <<b::bytes-size(1)>> <> <<c::bytes-size(1)>> <> "02",
-          p1,
-          p2,
-          dest | _
-        ],
-        %{mem: mem, ptr: ptr}
-      ) do
+  def process([{_a, b, c, "02"}, p1, p2, dest | _], %{mem: mem, ptr: ptr}) do
     mem = compute(mem, {c, p1}, {b, p2}, dest, &Kernel.*/2)
     {:ok, %{mem: mem, ptr: ptr + 4}}
   end
 
-  def process(["00003", dest | _], %{mem: mem, ptr: ptr}) do
+  def process([{_a, _b, _c, "03"}, dest | _], %{mem: mem, ptr: ptr}) do
     mem = input(mem, dest)
     {:ok, %{mem: mem, ptr: ptr + 2}}
   end
 
-  def process(
-        [
-          <<_a::bytes-size(1)>> <> <<_b::bytes-size(1)>> <> <<c::bytes-size(1)>> <> "04",
-          source | _
-        ],
-        %{mem: mem, ptr: ptr}
-      ) do
+  def process([{_a, _b, c, "04"}, source | _], %{mem: mem, ptr: ptr}) do
     mem = output(mem, {c, source})
     {:ok, %{mem: mem, ptr: ptr + 2}}
   end
 
-  def process(
-        [
-          <<_a::bytes-size(1)>> <> <<b::bytes-size(1)>> <> <<c::bytes-size(1)>> <> "05",
-          check,
-          jmp | _
-        ],
-        state
-      ) do
+  def process([{_a, b, c, "05"}, check, jmp | _], state) do
     state = jump_true(state, {c, check}, {b, jmp})
     {:ok, state}
   end
 
-  def process(
-        [
-          <<_a::bytes-size(1)>> <> <<b::bytes-size(1)>> <> <<c::bytes-size(1)>> <> "06",
-          check,
-          jmp | _
-        ],
-        state
-      ) do
+  def process([{_a, b, c, "06"}, check, jmp | _], state) do
     state = jump_false(state, {c, check}, {b, jmp})
     {:ok, state}
   end
 
-  def process(
-        [
-          <<_a::bytes-size(1)>> <> <<b::bytes-size(1)>> <> <<c::bytes-size(1)>> <> "07",
-          p1,
-          p2,
-          dest | _
-        ],
-        %{mem: mem, ptr: ptr}
-      ) do
+  def process([{_a, b, c, "07"}, p1, p2, dest | _], %{mem: mem, ptr: ptr}) do
     mem = set_boolean(mem, {c, p1}, {b, p2}, dest, &Kernel.</2)
     {:ok, %{mem: mem, ptr: ptr + 4}}
   end
 
-  def process(
-        [
-          <<_a::bytes-size(1)>> <> <<b::bytes-size(1)>> <> <<c::bytes-size(1)>> <> "08",
-          p1,
-          p2,
-          dest | _
-        ],
-        %{mem: mem, ptr: ptr}
-      ) do
+  def process([{_a, b, c, "08"}, p1, p2, dest | _], %{mem: mem, ptr: ptr}) do
     mem = set_boolean(mem, {c, p1}, {b, p2}, dest, &Kernel.==/2)
     {:ok, %{mem: mem, ptr: ptr + 4}}
   end
@@ -131,6 +79,10 @@ defmodule Advent.Day5b do
   def pad(opcode) when opcode < 1000, do: "00#{opcode}"
   def pad(opcode) when opcode < 10000, do: "0#{opcode}"
   def pad(opcode), do: to_string(opcode)
+
+  def parse(<<a::bytes-size(1)>> <> <<b::bytes-size(1)>> <> <<c::bytes-size(1)>> <> opcode) do
+    {a, b, c, opcode}
+  end
 
   def compute(mem, p1, p2, dest, operation) do
     value = operation.(lookup(mem, p1), lookup(mem, p2))
